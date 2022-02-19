60ead0be4dca8e271a091cf3165ca37e6c355015
Jonathan Sutton <jonny.a.sutton@gmail.com>
2022-02-19 10:26:34 +0000

    add types and args

--- src/main.py
+++ src/main.py
@@ -1,15 +1,25 @@
+from typing import List, Dict
+from genanki import Deck
 
-
-def main():
+def main(args: List[str]) -> int:
+    # Scrape the data from Wikidata
     data = scrape_wikidata()
+    # Build the deck object
     deck = build_deck(data)
+    # Write the deck object to an .apkg file
     write_deck(deck)
+    return 0
 
-def scrape_wikidata():
+def scrape_wikidata() -> List[Dict[str, str]]:
     pass
 
-def build_deck(data):
+def build_deck(data: List[Dict[str, str]]) -> Deck:
+    pass
+
+def write_deck(deck: Deck) -> None:
     pass
 
 if __name__ == "__main__":
-    main()
+    import sys
+    status = main(sys.argv)
+    sys.exit(status)
