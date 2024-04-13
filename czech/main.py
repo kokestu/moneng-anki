ff5995fd8799ffe109b146770c51043f4a22327e
Jana Smutna <jana@poly-ai.com>
2024-04-13 16:33:10 +0100

    add translator functions

--- czech/main.py
+++ czech/main.py
@@ -42,8 +42,35 @@ def main(args: List[str]) -> int:
 def scrape_wiktionary(page: str) -> Data:
     pass
 
+
+def _get_word_translation(text):
+    with open("deepl-api.txt") as f:
+        api_key = f.readline()
+    url = 'https://api-free.deepl.com/v2/translate'
+    headers = {
+        'Authorization': f'DeepL-Auth-Key {api_key}'
+    }
+    data = {
+        'text': text,
+        "source_lang": 'CS',
+        'target_lang': 'EN',
+        'tag_handling': 'html',
+        'split_sentences': 0
+    }
+    response = requests.post(url, headers=headers, data=data)
+    return response.json()["translations"][0]["text"]
+
+
 def get_translations(data: Data):
-    pass
+    """Translate example and fill in example_en for each value in data
+
+    Args:
+        data (Data): The word data
+    """
+    for entry in data.values():
+        for definition in entry.defs:
+            definition.example_en = _get_word_translation(definition.example)
+
 
 def get_tts(data: Data):
     pass
